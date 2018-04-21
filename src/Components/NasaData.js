3cbf745273392ee619f126d0defb4a8f6c124e8b
ali1129js <ali1129js@gmail.com>
2018-04-21 22:48:25 +0200

    minor changes

--- src/Components/NasaData.js
+++ src/Components/NasaData.js
@@ -1,3 +1,9 @@
+/**
+ * @Author: Ali Ismail
+ * @Date:   2018-04-21T22:20:48+02:00
+ * @Last modified by:   Ali Ismail
+ * @Last modified time: 2018-04-21T22:21:09+02:00
+ */
 import React, { Component } from 'react';
 
 class NasaData extends Component {
@@ -7,7 +13,7 @@ class NasaData extends Component {
       <div><strong>{this.props.data.title}</strong></div>,
       <div><strong>{this.props.data.date}</strong></div>,
       <div><img title="content" className="image"
-        style={{width: 1024 , height: 576 }}
+        style={{width: 800 , height: 576 }}
         src={this.props.data.url} alt="img" /></div>
       <div><p>{this.props.data.explanation}</p></div>
     </div>
