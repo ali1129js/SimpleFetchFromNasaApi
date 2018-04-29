638de430c1b6aa0b013359125d65657b63f61a79
ali1129js <ali1129js@gmail.com>
2018-04-29 15:08:36 +0200

    iframe updated

--- src/Components/NasaData.js
+++ src/Components/NasaData.js
@@ -1,8 +1,8 @@
 /**
  * @Author: Ali Ismail
  * @Date:   2018-04-21T22:20:48+02:00
- * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-21T22:21:09+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-04-29T15:07:39+02:00
  */
 import React, { Component } from 'react';
 
@@ -12,12 +12,12 @@ class NasaData extends Component {
     <div className="NasaData">
       <div><strong>{this.props.data.title}</strong></div>,
       <div><strong>{this.props.data.date}</strong></div>,
-      <div><img title="content" className="image"
+      <div><iframe title="content" className="image"
         style={{width: 800 , height: 576 }}
         src={this.props.data.url} alt="img" /></div>
       <div><p>{this.props.data.explanation}</p></div>
     </div>
-        );
-        }
+      );
+    }
 }
 export default NasaData;
