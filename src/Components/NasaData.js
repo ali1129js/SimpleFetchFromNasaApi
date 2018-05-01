866c31bf226e7e71b296262dd2af97405d90d4d6
ali1129js <ali1129js@gmail.com>
2018-05-01 14:43:00 +0200

    destructuring and conditional img/video

--- src/Components/NasaData.js
+++ src/Components/NasaData.js
@@ -2,22 +2,38 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-21T22:20:48+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-04-29T15:07:39+02:00
+ * @Last modified time: 2018-05-01T14:35:12+02:00
  */
 import React, { Component } from 'react';
 
 class NasaData extends Component {
   render() {
-    return (
-    <div className="NasaData">
-      <div><strong>{this.props.data.title}</strong></div>,
-      <div><strong>{this.props.data.date}</strong></div>,
-      <div><iframe title="content" className="image"
-        style={{width: 800 , height: 576 }}
-        src={this.props.data.url} alt="img" /></div>
-      <div><p>{this.props.data.explanation}</p></div>
-    </div>
-      );
+    //Destructuring ES6
+    const { title, date, explanation, media_type} = this.props.data
+    if (media_type === "image"){
+      return (
+      <div className="NasaData">
+        <div><strong>{title}</strong></div>,
+        <div><strong>{date}</strong></div>,
+        <div><img title="content" className="image"
+          style={{width: 'auto' , height: 'auto'}}
+          src={this.props.data.url} alt="img" /></div>
+        <div><p>{explanation}</p></div>
+      </div>
+      )
     }
+    else {
+      return (
+        <div className="NasaData">
+          <div><strong>{title}</strong></div>,
+          <div><strong>{date}</strong></div>,
+          <div><iframe title="content" className="image"
+            style={{width: 'auto' , height: 'auto'}}
+            src={this.props.data.url} alt="img" /></div>
+          <div><p>{explanation}</p></div>
+        </div>
+      )
+    }
+  }
 }
 export default NasaData;
