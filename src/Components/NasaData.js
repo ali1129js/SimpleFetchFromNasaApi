52d07752a882e09c1087059dfc4dd0ac50ccd730
ali1129js <ali1129js@gmail.com>
2018-05-05 15:01:14 +0200

    more bootstrap classes

--- src/Components/NasaData.js
+++ src/Components/NasaData.js
@@ -2,7 +2,7 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-21T22:20:48+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-05T14:43:19+02:00
+ * @Last modified time: 2018-05-05T14:59:52+02:00
  */
 import React, { Component } from 'react'
 class NasaData extends Component {
@@ -11,15 +11,15 @@ class NasaData extends Component {
     const { title, date, explanation, media_type, url} = this.props.data
     if (media_type === "image"){
       return (
-      <div className="card text-center">
+      <div className="card border-primary bg-dark  mb-3 text-center">
         <h5 class="card-header">
-          <div className="card-title">{title}</div>
-          <div>{date}</div>
+          <div className="card-title text-white">{title}</div>
+          <div className="card-title text-white">{date}</div>
         </h5>
         <div className="card-img-top">
           <img className="card-img-top"
             src={url} alt="img" /></div><br />
-        <div className="card-body"><p>{explanation}</p></div>
+        <div className="card-body text-danger">{explanation}</div>
       </div>
       )
     }
