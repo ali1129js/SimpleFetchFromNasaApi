0d9a52c3fcedb1992531b1c9c5c49d1f7e99b0d0
ali1129js <ali1129js@gmail.com>
2018-05-08 16:36:47 +0200

    fixed logo link to nasa's website

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,15 +2,20 @@
  * @Author: Ali
  * @Date:   2018-05-06T15:44:46+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-06T18:40:33+02:00
+ * @Last modified time: 2018-05-08T16:35:35+02:00
  */
 import React from 'react'
 const Navbar = () =>
   <nav className="navbar sticky-top navbar-dark bg-dark">
     <div className="row">
       <div className="navbar-header">
-        <img className="navbar-brand" src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg" alt="logo" width="50" height="50"/>
-        <a className="navbar-brand" href="https://www.nasa.gov" target="_blank" rel="noopener noreferrer">Nasa Today</a>
+        <a className="navbar-brand" href="https://www.nasa.gov" target="_blank" rel="noopener noreferrer">
+        <img
+          className="navbar-brand" src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg"
+          alt="logo"
+          width="50"
+          height="50"/>
+        Nasa Today</a>
       </div>
     </div>
   </nav>
